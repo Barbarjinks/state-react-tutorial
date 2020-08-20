888ae46b6edea5c62d01aeb0d1be968de1e08eee
Cai Jenkins <Caisjenkins@gmail.com>
2020-08-20 10:41:47 +0100

    fixed bugs - going below zero

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,7 +6,8 @@ const currencyOptions = {
     maximumFractionDigits: 2,
 }
 
-    function getTotal(total) {
+function getTotal(cart) {
+    const total = cart.reduce((totalCost, item) => totalCost + item.price, 0);
     return total.toLocaleString(undefined, currencyOptions)
 }
 
@@ -31,37 +32,29 @@ const products = [
 function cartReducer(state, action) {
     switch(action.type) {
         case 'add':
-            return [...state, action.name];
+            return [...state, action.product];
         case 'remove':
+            const productIndex = state.findIndex(item => item.name === action.product.name);
+            if(productIndex < 0) {
+                return state;
+            }
             const update = [...state];
-            update.splice(update.indexOf(action.name), 1);
+            update.splice(productIndex, 1);
             return update;
         default:
             return state;
     }
 }
 
-function totalReducer (state, action) {
-    if(action.type === 'add') {
-        return state + action.price;
-    }
-    return state - action.price
-}
-
 export default function Product() {
     const [cart, setCart] = useReducer(cartReducer, []);
-    const [total, setTotal] = useReducer(totalReducer, 0);
 
     function add(product) {
-        const { name, price } = product;
-        setCart({ name, type: 'add'});
-        setTotal({ price, type: 'add' });
+        setCart({ product, type: 'add' });
     }
 
     function remove(product) {
-        const { name, price } = product;
-        setCart({ name, type: 'remove' });
-        setTotal({ price, type: 'remove' });
+        setCart({ product, type: 'remove' });
         }
 
     return(
@@ -69,7 +62,7 @@ export default function Product() {
             <div>
                 Shopping Cart: {cart.length} total items.
             </div>
-            <div>Total: £{getTotal(total)} </div>
+            <div>Total: £{getTotal(cart)} </div>
             <div>
             {products.map(product => (
                 <div key={product.name}>
